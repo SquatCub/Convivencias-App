00894e8674528557d0411f89143b4c8b2e158778
squatcub <brandon.alexis10@hotmail.com>
2021-04-12 18:08:31 -0500

    Primera implementacion al diseño principal con tarjetas

--- resources/views/principal/index.blade.php
+++ resources/views/principal/index.blade.php
@@ -10,11 +10,41 @@
     <h1>Iniciaste sesion como usuario</h1>
     @endif
 
-    @foreach($categorias as $categoria)
-    {{$categoria->nombre}}
-    <br>
-    @endforeach
-
-</div>
+    <div class="row">
+        @foreach($categorias as $categoria)
+        <div class="col-md-4">
+            <div class="wrapper">
+                <div class="container">
+                    <div class="top" style="background: url('/images/{{$categoria->imagen}}') no-repeat center center;"></div>
+                        <div class="bottom">
+                            <div class="left">
+                                <div class="details">
+                                <h1>{{$categoria->nombre}}</h1>
+                                <p class="desc">{{$categoria->descripcion}}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+<script>
+    desc = document.querySelectorAll('.desc');
+    desc.forEach(element=> {
+        var aux = "";
+        var s = element.innerHTML;
+        var i = 0;
+        if (s.length < 15) {
+            aux+=s;
+        } else {
+            for (i = 0; i < 15; i++) {
+                aux+= s[i];
+            }
+        }
+        aux += '...';
+        element.innerHTML = aux;
+    });
+</script>   
 
 @endsection
